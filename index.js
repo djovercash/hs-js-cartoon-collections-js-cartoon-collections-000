425785e2784f8451a958e828f21330307ebb580f
Drew Overcash <drewovercash@gmail.com>
2017-11-04 16:50:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+var dwarves = ["Dopey", "Grumpy", "Bashful"];
+
 function dwarfRollCall(dwarves) {
-  var dwarves = ["Dopey", "Grumpy", "Bashful"];
   for (var i = 0; i < dwarves.length; i++) {
     document.write((i+1) + ". " + dwarves[i] + " ");
   };
