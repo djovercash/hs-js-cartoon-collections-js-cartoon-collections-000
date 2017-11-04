a451a9b3b087c7d9cfa2f48cd4112662697425cf
Drew Overcash <drewovercash@gmail.com>
2017-11-04 17:10:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var dwarves = ["Dopey", "Grumpy", "Bashful"];
 
 function dwarfRollCall(dwarves) {
   for (var i = 0; i < dwarves.length; i++) {
-    console.log((i+1) + '. ' + dwarves[i] + " ");
+    stringDwarves.push((i+1) + '. ' + dwarves[i] + " ");
   };
+  return stringDwarves.join();
 };
 
 function summonCaptainPlanet(planeteerCalls){
